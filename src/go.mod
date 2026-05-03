8b4c83d942f0989dc63f315f34cdbb063b1802c0
carolinegoulart <goulartscaroline@gmail.com>
2026-05-03 15:18:54 -0300

    Atualiza workflow do GitHub Actions

---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module github.com/carolinegoulart/github-actions-poc
+
+go 1.22
---
+++ src/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 5000
+
+// Handler for the root path
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "Request received on path: %s\n", r.URL.Path)
+}
+
+// Main function to start the HTTP server
+func main() {
+	http.HandleFunc("/", index)
+	addr := fmt.Sprintf("0.0.0.0:%d", port)
+	log.Printf("Go app running on: %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
